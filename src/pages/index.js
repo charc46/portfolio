f03aa9576e5cda89a678745706fde7e86d79a868
Charles Crane <charlie.crane46@gmail.com>
2021-03-10 10:19:47 +0000

    Add some mobile responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,12 +11,13 @@ const Home = () => {
     <Layout>
       <Head title='Home'/>
       <div className={indexStyles.indexContainer}>
-        <h2>Hello.<br /> I'm Charles, a Full Stack Web Developer from London.</h2>
+        <h1>Hello.</h1>
+        <h3>I'm Charles, a Full Stack Web Developer from London.</h3>
         <p>I have experience with Ruby on Rails and I have recently been working more with Javascript and React.
           I have been delving further into the Javascript ecosystem learning more about Full Stack Development with Javascript.<br /> I am eager to carry on learning more about Javascript development as well as improving my Ruby on Rails skills.<br />
           <br />
           I love food. I love coffee. I love to travel. And of course I love all things techy.</p>
-          <p>Take a look at some of my <Link className={indexStyles.link} to='/projects'>projects</Link> and feel free to <Link className={indexStyles.link} to='/contact'>send me a message</Link> if you have any questions!</p>
+          <p>Take a look at some of my <Link className={indexStyles.link} to='/projects'><strong>projects</strong></Link> and feel free to <Link className={indexStyles.link} to='/contact'><strong>send me a message</strong></Link> if you have any questions!</p>
       </div>
     </Layout>
   )
