97d8ee63cd236dd9b27ac7d72e87f94fcb2631d8
Charles Crane <charlie.crane46@gmail.com>
2021-04-11 14:16:28 +0100

    Change up colours

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,12 +9,13 @@ const Header = () => {
   return (
     <header className={headerStyles.header}>
       <Navbar expand="lg" className={headerStyles.nav} variant="dark">
-        <h2 className={headerStyles.navTitle}><Link to='/'>Charles<br/>Crane<br/>Dev</Link></h2>
+        <h2 className={headerStyles.navTitleMobile}><Link to='/'>Charles<br/>Crane<br/>Dev</Link></h2>
         <Navbar.Toggle aria-controls="basic-navbar-nav" id={headerStyles.toggler} />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className={`${headerStyles.navList}`}>
             <Link to='/' id={headerStyles.link} className={`${headerStyles.navLinks} nav-link`} activeClassName={headerStyles.active}>Home</Link>
             <Link to='/projects' id={headerStyles.link} className={`${headerStyles.navLinks} nav-link`} activeClassName={headerStyles.active}>Projects</Link>
+            <h2 className={headerStyles.navTitle}><Link to='/'>Charles<br/>Crane<br/>Dev</Link></h2>
             <Link to='/contact' id={headerStyles.link} className={`${headerStyles.navLinks} nav-link`} activeClassName={headerStyles.active}>Contact</Link>
             <Link to='/blog' id={headerStyles.link} className={`${headerStyles.navLinks} nav-link`} activeClassName={headerStyles.active}>Blog</Link>
           </Nav>
