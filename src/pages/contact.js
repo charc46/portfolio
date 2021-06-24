89d21010a8711a24e936da9b56ea6ac6ab69c99d
Charles Crane <charlie.crane46@gmail.com>
2021-06-24 07:49:52 +0100

    Add netlify form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,15 +10,12 @@ const contact = () => {
       <Head title='Contact'/>
       <div className={contactStyles.container}>
         <h2 className={contactStyles.title}>Contact Me</h2>
-        <form action="https://formsubmit.co/75ed5f5db89f263d8d669d44e761e8b5" method="POST" >
+        <form name="contact" method="POST" data-netlify="true" action='/thanks' >
           <input type="name" name="name" placeholder='Your Name' className={contactStyles.name} />
           <input type="subject" name="subject" placeholder='Subject' className={contactStyles.name} />
           <input type="email" name="email" placeholder='Your Email' className={contactStyles.email} />
           <textarea name='message' rows='5' cols='20' placeholder='Talk to me..' className={contactStyles.message} />
           <button className={contactStyles.button}>Send</button>
-          <input type="text" name="_honey" style={{display:'none'}} />
-          <input type="hidden" name="_captcha" value="false"></input>
-          <input type="hidden" name="_next" value="https://www.charlescrane.co.uk/thanks"></input>
         </form>
       </div>
     </Layout>
